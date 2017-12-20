1f1b22eaf54224d6163c86c9ab5c3a57c175eae2
Lucas Noblat <lucasnoblat123@gmail.com>
2017-12-20 21:06:38 -0200

    Allowed Host *

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '-uhpfb-=5&l!s0h5sn8*(3a^slvqng24z7b$0yt!a8w+2*$m9#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
